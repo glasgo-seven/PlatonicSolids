e46cb84781de109abc5127754e79f56518f00ff9
glasgo-seven <glasgoseven@gmail.com>
2023-03-17 13:55:25 +0300

    colored axises

--- lean/shape.py
+++ lean/shape.py
@@ -1,5 +1,43 @@
 from matrix import Matrix, Zero3_Vector
 
+COLORS = {
+	#				 r g b
+	'black'		:	(0,0,0),
+	'blue'		:	(0,0,1),
+	'green'		:	(0,1,0),
+	'cian'		:	(0,1,1),
+	'red'		:	(1,0,0),
+	'purple'	:	(1,0,1),
+	'yellow'	:	(1,1,0),
+	'white'		:	(1,1,1)
+}
+
+def load_axises(_position : tuple[float, float, float], _edge : float) -> tuple[list[Matrix], list[tuple[int]], list[tuple[int]], list[tuple[float, float, float]]]:
+	vertices = [
+		Matrix(_position, _is_vector = True),
+
+		Matrix((_position[0] + _edge, _position[1], _position[2]), _is_vector = True),
+		Matrix((_position[0] - _edge, _position[1], _position[2]), _is_vector = True),
+
+		Matrix((_position[0], _position[1] + _edge, _position[2]), _is_vector = True),
+		Matrix((_position[0], _position[1] - _edge, _position[2]), _is_vector = True),
+
+		Matrix((_position[0], _position[1], _position[2] + _edge), _is_vector = True),
+		Matrix((_position[0], _position[1], _position[2] - _edge), _is_vector = True)
+	]
+	edges = [
+		(1,2),
+		(3,4),
+		(5,6)
+	]
+	surfaces = []
+	colors = [
+		COLORS['red'],
+		COLORS['green'],
+		COLORS['blue']
+	]
+	return (vertices, edges, surfaces, colors)
+
 def load_triangle(_position : tuple[float, float, float], _edge : float) -> tuple[list[Matrix], list[tuple[int]], list[tuple[int]]]:
 	m = (_edge ** 2 * 3 / 4) ** (1 / 2)
 	vertices = [
@@ -33,14 +71,16 @@ def load_tetra():
 	pass
 
 SHAPES = {
+	'Axises'	:	load_axises,
 	'Triangle'	:	load_triangle,
 	'Square'	:	load_square,
 	'Cube'		:	load_cube,
 	'Tetra'		:	load_tetra,
 }
 
+
 class Shape:
-	def __init__(self, _position : Matrix = Zero3_Vector, _edge : float = 1, _shape_type = None) -> None:
+	def __init__(self, _position : Matrix = Zero3_Vector, _edge : float = 1, _shape_type = None, _color = COLORS['white']) -> None:
 		self.position	: Matrix	= _position
 		self.edge		: float		= _edge
 
@@ -52,6 +92,10 @@ class Shape:
 		self.vertices = shape_data[0]
 		self.edges = shape_data[1]
 		self.surfaces = shape_data[2]
+		try:
+			self.color 		: list[tuple[float, float, float]] = shape_data[3]
+		except:
+			self.color 		: tuple[float, float, float] = _color
 
 	def __repr__(self) -> str:
 		return f'pos: {self.position}\nedge: {self.edge}\nv: {self.vertices}\ne: {self.edges}\ns: {self.surfaces}\n'
